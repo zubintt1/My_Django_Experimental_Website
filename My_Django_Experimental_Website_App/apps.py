a773d5e9e3a3d8034612518c84231e70b026c1c6
[zubintt1] <[zubin_zorro@rediff.com]>
2016-04-01 20:02:16 +0530

    Added first application in the project

---
+++ My_Django_Experimental_Website_App/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class MyDjangoExperimentalWebsiteAppConfig(AppConfig):
+    name = 'My_Django_Experimental_Website_App'
