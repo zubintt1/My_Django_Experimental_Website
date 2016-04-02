d73aae90a0e4b473ba6c15bc2681f75bc113d389
[zubintt1] <[zubin_zorro@rediff.com]>
2016-04-02 10:10:31 +0530

    Some Setting Changes

--- My_Django_Experimental_Website/settings.py
+++ My_Django_Experimental_Website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'My_Django_Experimental_Website_App',
 ]
 
 MIDDLEWARE_CLASSES = [
